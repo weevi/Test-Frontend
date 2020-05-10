dbcdb8cb76ce986105a7d218735acebe45dcce75
paskeviciute.rima@gmail.com <paskeviciute.rima@gmail.com>
2020-05-10 22:35:31 +0100

    add mobile responsive form

--- src/components/FilterJson.js
+++ src/components/FilterJson.js
@@ -30,9 +30,7 @@ class FilterJson extends React.Component {
             items: result.passwords
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         (error) => {
           this.setState({
             isLoaded: true,
